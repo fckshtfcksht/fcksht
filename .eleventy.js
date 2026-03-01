679bc38285579dc1a79c607559a15b7be996e149
fckshtfcksht <1993.fcksht@gmail.com>
2026-03-01 15:30:08 -0800

    Add shop collection

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addCollection("shop", function(collectionApi) {
+    return collectionApi.getFilteredByTag("shop");
+  });
   return {
     dir: { input: ".", output: "_site" }
   };
